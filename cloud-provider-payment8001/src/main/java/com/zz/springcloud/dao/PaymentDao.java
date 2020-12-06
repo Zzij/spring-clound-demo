23610aa068cc12f0dc9c8d7fc0a0e66b60c968f9
zzj <1241646494@qq.com>
2020-12-06 21:16:19 +0800

    eureka集群

---
+++ cloud-provider-payment8001/src/main/java/com/zz/springcloud/dao/PaymentDao.java
@@ -0,0 +1,13 @@
+package com.zz.springcloud.dao;
+
+import com.zz.springcloud.entities.Payment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+@Mapper
+public interface PaymentDao {
+
+    int create(Payment payment);
+
+    Payment getPaymentById(@Param("id") long id);
+}
